873330ad0a99580d81c93c80dd9e404d2d872287
garyo <surpace2479@gmail.com>
2020-01-15 16:21:03 +0900

    update: データの修正

--- data/ComparisonLiDAR/main.py
+++ data/ComparisonLiDAR/main.py
@@ -30,14 +30,23 @@ p2_R = np.genfromtxt(file_name, delimiter=',', filling_values = 0)
 x_R = p2_R[0:, 0]
 y_R = p2_R[0:, 1]
 
+# この日の日没と日の出の日時
+sunset_time = datetime.fromtimestamp(1578814380 + 9*60*60)
+sunrise_time = datetime.fromtimestamp(1578866040 + 9*60*60)
+sunset_line = [[sunset_time, sunset_time], [0.58, 0.61]]
+sunrise_line = [[sunrise_time, sunrise_time], [0.58, 0.61]]
+
 # タイムスタンプのリストを作成
 ts = [0]*len(x_L)
 for i in range(len(x_L)):
-    ts[i] = datetime.fromtimestamp(x_L[i])
+    ts[i] = datetime.fromtimestamp(x_L[i] + 9*60*60)
+    # ts[i] = datetime.fromtimestamp(x_L[i])
 
 # グラフにデータを追加
 ax.plot(ts, y_L, color = color_list[0], linewidth=0.5, label='LiDAR')
 ax.plot(ts, y_R, color = color_list[1], linewidth=0.5, label='RADAR')
+ax.plot(sunset_line[0], sunset_line[1], color = color_list[2], linewidth=2, label='sunset_line')
+ax.plot(sunrise_line[0], sunrise_line[1], color = color_list[3], linewidth=2, label='sunrise_line')
 
 # 日付ラベルフォーマットを修正
 ax.xaxis.set_major_locator(mdates.HourLocator(byhour=range(0, 24, 3), tz=None))
@@ -52,7 +61,7 @@ plt.setp(labels, rotation=45, fontsize=10)
 # plt.title('')
 
 # グラフ範囲
-# plt.xlim(datetime.fromtimestamp(x_L[0] + 9*60*60), datetime.fromtimestamp(x_L[len(x_L)-1] + 9*60*60))
+# plt.xlim()
 # plt.ylim(0.0, 0.7)
 
 # 余白設定
